8ce93a6ba669bd91ba3e5f4115da950735ef47f9
Ted Ogmundson <togmund@gmail.com>
2019-10-21 23:07:16 +0000

    refactored middle and tail with assert.DeepEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,17 +1,17 @@
 const middle = require("../src/middle");
-const assertArraysEqual = require("../src/assertArraysEqual");
+const assert = require("chai").assert;
 
 // Tests
 
 describe("middleEqualTest", () => {
   it("should return no value when the length of the array is fewer than three", () => {
-    assertArraysEqual(middle([0, 78567]), []); // => false
+    assert.deepEqual(middle([0, 78567]), []); // => true
   });
   it("should return the middle value when the length of the array is odd", () => {
-    assertArraysEqual(middle([0, 2, 6, 8, 10]), [6]); // => true
+    assert.deepEqual(middle([0, 2, 6, 8, 10]), [6]); // => true
   });
   it("should return the average of the middle two values when the array length is even", () => {
-    assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => true
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => true
   });
 });
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,17 @@
 const tail = require("../src/tail");
-// const assertEqual = require("../src/assertEqual");
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-const assertDeepEqual = require("chai").assert;
+const assert = require("chai").assert;
 
 // Test Case 1: Check the returned array elements
 describe("TailTest", () => {
   it("should return all the elements in the array except the first element", () => {
-    assertDeepEqual(tail([5, 6, 7]), [6, 7]); // Should return TRUE
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]); // Should return TRUE
   });
 
-  it("should return undefined when there is only one item in the array", () => {
-    assertDeepEqual(tail(["Hello"]), undefined); // Should return FALSE
+  it("should return an empty array when there is only one item in the array", () => {
+    assert.deepEqual(tail(["Hello"]), []); // Should return FALSE
   });
 
-  it("should return undefined when there is nothing in the array", () => {
-    assertDeepEqual(tail([]), undefined); // Should return FALSE
+  it("should return an empty array when there is nothing in the array", () => {
+    assert.deepEqual(tail([]), []); // Should return FALSE
   });
 });
