a1b925283b60ac6b35b9870695fd305c93171cde
Ted Ogmundson <togmund@gmail.com>
2019-10-21 21:44:36 +0000

    tsting merge stuff

--- src/assertEqual.js
+++ src/assertEqual.js
@@ -2,13 +2,8 @@ const chalk = require("chalk");
 
 const assertEqual = function(actual, expected) {
   actual === expected
-<<<<<<< HEAD
-    ? console.log(chalk.green(`Assertion Passed: ${actual} === ${expected}`))
-    : console.log(chalk.green(`Assertion Failed: ${actual} !== ${expected}`));
-=======
     ? console.log(`Matched: ${actual} === ${expected}`)
     : console.log(`Not Matched: ${actual} !== ${expected}`);
->>>>>>> 031ec17b631102a1d10024e0f0dd86cf6e855106
 };
 
 module.exports = assertEqual;
