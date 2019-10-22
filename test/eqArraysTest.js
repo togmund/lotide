69aeda213d0fe6da48d7678a65e2686307fca475
Ted Ogmundson <togmund@gmail.com>
2019-10-22 01:59:53 +0000

    refactored eqArraysTest touse Deep & Strict equal and match human-readable testability

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -4,10 +4,13 @@ const assert = require("chai").assert;
 // TEST CODE
 
 describe("eqArrays", () => {
-  it("", () => {
-    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+  it("should take in two arrays of numbers and return true based on a perfect match", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("should take in two arrays of strings and return true based on a perfect match", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+  });
+  it("should take in two arrays that are not perfectly equal and return false", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false); 
   });
 });
